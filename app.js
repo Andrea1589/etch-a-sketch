4589e39d4280232cd26d2516396364844b02577c
Andrea1589 <andreaecs@gmail.com>
2022-10-30 21:55:22 -0500

    Color squares when the mouse is down and moving

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const grid = document.getElementById("grid");
 const numSquares = 16;
 
-console.log(numSquares);
-
 for (let i = 0; i < numSquares; i++) {
     const row = document.createElement("div");
     row.className = "row";
     grid.appendChild(row);
+    
     for (let j = 0; j < numSquares; j++) {
         const square = document.createElement("div");
         square.className = "square";
         row.appendChild(square);
+    }
+}
+
+document.onmousedown = function(e){
+    document.onmousemove = function(e){
+        if (e.target.className === "square") {
+            e.target.style.backgroundColor = "black";
+        }
+    }
+}
+
+document.onmouseup = function(e){
+    document.onmousemove = function(e){
+        if (e.target.className === "square") {
+            e.target.style.backgroundColor = "none";
+        }
     }
 }
