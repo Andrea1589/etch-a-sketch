6839ff2331347cfbe9447003efb6032380d26ce2
Andrea Carrion <andreaecs@gmail.com>
2022-11-02 15:53:33 -0500

    Add color options for the pen: Rainbow, Cold and Warm

--- app.js
+++ app.js
@@ -1,21 +1,52 @@
-setGrid(16);
+let colorPalette = getColors('black');
 
-document.onmousedown = function(e){
-    document.onmousemove = function(e){
-        if (e.target.className === "square") {
-            e.target.style.backgroundColor = "black";
-            e.target.style.borderColor = "gray";
+setGrid(16);
+drawSketch();
+
+// function blackPencil(){
+//     document.onmousedown = function(e){
+//         document.onmouseover = function(e){
+//             if (e.target.className === "square") {
+//                 e.target.style.backgroundColor = "black";
+//                 e.target.style.borderColor = "black";
+//             }
+//         }
+//     }
+//     document.onmouseup = function(e){
+//         document.onmouseover = function(e){
+//             if (e.target.className === "square") {
+//                 e.target.style.backgroundColor = "none";
+//                 e.target.style.borderColor = "none";
+//             }
+//         }
+//     }
+// }
+
+function drawSketch(){
+    document.onmousedown = function(e){
+        document.onmouseover = function(e){
+            if (e.target.className == "square") {
+                let number = getRandomInt(0,colorPalette.length-1);
+                //console.log(number);
+                e.target.style.backgroundColor = colorPalette[number];
+                e.target.style.borderColor = colorPalette[number];
+            }
+        }
+    }
+    document.onmouseup = function(e){
+        document.onmouseover = function(e){
+            if (e.target.className === "square") {
+                e.target.style.backgroundColor = "none";
+                e.target.style.borderColor = "none";
+            }
         }
     }
 }
 
-document.onmouseup = function(e){
-    document.onmousemove = function(e){
-        if (e.target.className === "square") {
-            e.target.style.backgroundColor = "none";
-            e.target.style.borderColor = "none";
-        }
-    }
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function showGrid(numSquares){
@@ -46,4 +77,38 @@ function setGrid(numSquares){
 function clearGrid(){
     const gridSize = document.getElementById("grid-range").value;
     setGrid(gridSize);
+}
+
+function setColors(){
+    const option = document.querySelector('input[name="color"]:checked');
+    //console.log(option.value);
+    if (option) {
+        colorPalette = getColors(option.value);
+    }
+}
+
+function getColors(userSelection){
+    let colors = undefined;
+
+    switch (userSelection) {
+        case "black":
+            colors=['black'];
+            break;
+
+        case "rainbow":
+            colors=['#ff0000', '#fc4444', '#fc6404', '#fcd444', '#8cc43c', '#029658', '#1abc9c', '#5bc0de', '#6454ac', '#fc8c84'];
+            break;
+
+        case "cold":
+            colors=['#344464', '#548ca4', '#549cac', '#2c445c', '#a4ccd4', '#acbccc', '#b4c4d4', '#acd4cc', '#5c8ca4', '#52b3d0'];
+            break;
+
+        case "warm":
+            colors=['#793f0d', '#ac703d', '#e49969', '#6e7649', '#9d9754', '#dfd27c', '#dbca69', '#855723', '#eec59a', '#c7c397'];
+            break;
+                    
+        default:
+            break;
+    }
+    return(colors);
 }
